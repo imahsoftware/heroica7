507a6e483b27ed40922f46c37c5aca0eb2bce113
Andres Felipe Pelaez <andres.computo@gmail.com>
2026-05-14 08:43:15 -0500

    Migraciones personas

--- app/models/egresosimagen.rb
+++ app/models/egresosimagen.rb
@@ -2,10 +2,10 @@ class Egresosimagen < ApplicationRecord
   belongs_to :egreso
   belongs_to :user, optional: true
 
-  has_attached_file :docfile,
+  has_attached_file :egreso,
                     use_timestamp: false
-  validates_attachment_content_type :docfile, content_type: /\A.*\/.*\Z/
-  validates :docfile, attachment_presence: true, presence: true
+  validates_attachment_content_type :egreso, content_type: /\A.*\/.*\Z/
+  validates :egreso, attachment_presence: true, presence: true
 
   validates :descripcion, presence: { message: '* Obligatorio' }
 end
